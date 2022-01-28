028061c1a570b332027a86f38b176c1bd959e749
jacek <jacek.miloszewski@gmail.com>
2022-01-28 23:33:40 +0000

    zxc

--- deployment/k8s/deploy.py
+++ deployment/k8s/deploy.py
@@ -4,6 +4,7 @@ import json
 from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader
+import docker
 
 SCRIPT_DIR = Path(__file__).parent.absolute()
 ROOT_DIR = SCRIPT_DIR.parent.parent.absolute()
@@ -13,11 +14,8 @@ WEB_DIR = ROOT_DIR / "services" / "web"
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "env",
-        choices=["prod", "staging"],
-        help="Allowed values: prod, staging.",
-    )
+    parser.add_argument("env", choices=["prod", "staging"], help="Allowed values: prod, staging.")
+    parser.add_argument("--build", help="Build docker image.", action="store_true")
     return parser.parse_args()
 
 
@@ -46,13 +44,28 @@ def encode64(x):
     return {k: base64.b64encode(v.encode()).decode() for k, v in x.items()}
 
 
+def build_image():
+    client = docker.from_env()
+    _, logs = client.images.build(
+        path=str(WEB_DIR),
+        tag="hammergen",
+        dockerfile="Dockerfile"
+    )
+
+    for log in logs:
+        for k, v in log.items():
+            print(v, end="") if k == "stream" else print(v)
+
+
 if __name__ == "__main__":
     ARGS = parse_arguments()
+
+    if ARGS.build:
+        build_image()
+
     DEPLOY_CONFIG = read_deployment_config(ARGS.env)
 
     hydrate_template("namespace.yaml", ARGS.env)
     hydrate_template("configmap.yaml", ARGS.env, DEPLOY_CONFIG["web_env_vars"])
     hydrate_template("secret.yaml", ARGS.env, encode64(DEPLOY_CONFIG["web_env_vars"]))
     hydrate_template("deployment.yaml", ARGS.env)
-
-
