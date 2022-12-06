8d0a67558d04239192768109f98064180e110458
jacek <jacek.miloszewski@protonmail.com>
2022-12-06 22:44:25 +0000

    improved restore script

--- migration/restore.sh
+++ migration/restore.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mongorestore --archive=hammergen --gzip --uri="$1"
+name=hammergen_$(date +%d_%m_%Y)
+mongorestore --uri="$1" --nsFrom="hammergen.*" --nsTo="test.*" --archive="$name" --gzip
