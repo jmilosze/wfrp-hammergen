70e001c824128e522740e6561516cc68637a4167
jacek <jacek.miloszewski@protonmail.com>
2023-02-24 23:56:40 +0000

    feat: finished register user

--- src/frontend/src/utils/userValidators.js
+++ src/frontend/src/utils/userValidators.js
@@ -28,6 +28,13 @@ export function passwordErrors(password, passwordConfirmation) {
   return errors;
 }
 
+function checkString(testValue, label, regex, msg) {
+  if (regex.test(testValue)) {
+    return [true, null];
+  }
+  return [false, msg];
+}
+
 export function nameErrors(name) {
   let errors = [];
 
@@ -38,4 +45,27 @@ export function nameErrors(name) {
   }
 
   return errors;
+}
+
+const nameRe = /^.{1,20}$/;
+export function validUserName(name) {
+  return checkString(name, "Name", nameRe, "Name has to have between 1 and 20 characters.");
+}
+
+const emailRe =
+  /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/;
+export function validEmail(email) {
+  return checkString(email, "Email", emailRe, "Email address is invalid.");
+}
+
+const passwordRe = /^.{5,30}$/;
+export function validPassword(pswd) {
+  return checkString(pswd, "Password", passwordRe, "Name has to have between 5 and 30 characters.");
+}
+
+export function validPasswordMatch(pswd1, pswd2) {
+  if (pswd1 === pswd2) {
+    return [true, null];
+  }
+  return [false, "Passwords do not match."];
 }
