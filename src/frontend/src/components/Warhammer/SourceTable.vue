dab1208d270b07f4267022738919127a7217723c
jacek <jacek.miloszewski@protonmail.com>
2023-03-18 22:37:16 +0000

    started work on source table

---
+++ src/frontend/src/components/Warhammer/SourceTable.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-table :items="tableData" :fields="tableFields"></b-table>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const tableData = ref([
+  { name: "John", age: 30, city: "New York" },
+  { name: "Jane", age: 25, city: "San Francisco" },
+  { name: "Bob", age: 40, city: "Chicago" },
+]);
+
+const tableFields = ref([
+  { key: "name", label: "Name" },
+  { key: "age", label: "Age" },
+  { key: "city", label: "City" },
+]);
+</script>
+
+<style scoped></style>
