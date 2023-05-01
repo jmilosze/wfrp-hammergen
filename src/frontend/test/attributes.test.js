4bad3c96fd7c88217a9ec4d81137cbaa2b7512eb
jacek <jacek.miloszewski@protonmail.com>
2023-05-01 22:29:11 +0100

    test fixes

--- src/frontend/test/attributes.test.js
+++ src/frontend/test/attributes.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, test } from "vitest";
 import { getAttributes, sumAndMultAttr } from "../src/services/wh/attributes";
-
-import { species } from "@/services/wh/career";
+import * as c from "../src/services/wh/characterConstants";
+import { speciesWithRegion } from "../src/services/wh/character";
 
 test("sumAndMultAttr adds and multiplies attributes correctly", () => {
   let att1 = { WS: 20, BS: 20, S: 20, T: 20, I: 20, Ag: 20, Dex: 20, Int: 20, WP: 20, Fel: 20 };
@@ -27,34 +27,39 @@ test("sumAndMultAttr adds and multiplies attributes correctly", () => {
 describe("getRacialAttributes returns correct value", () => {
   test.each([
     {
-      name: species[0],
-      species: 0,
+      name: speciesWithRegion[c.HUMAN_REIKLAND],
+      species: c.HUMAN_REIKLAND,
       expected: { WS: 20, BS: 20, S: 20, T: 20, I: 20, Ag: 20, Dex: 20, Int: 20, WP: 20, Fel: 20 },
     },
     {
-      name: species[1],
-      species: 1,
+      name: speciesWithRegion[c.HALFLING_DEFAULT],
+      species: c.HALFLING_DEFAULT,
       expected: { WS: 10, BS: 30, S: 10, T: 20, I: 20, Ag: 20, Dex: 30, Int: 20, WP: 30, Fel: 30 },
     },
     {
-      name: species[2],
-      species: 2,
+      name: speciesWithRegion[c.DWARF_DEFAULT],
+      species: c.DWARF_DEFAULT,
       expected: { WS: 30, BS: 20, S: 20, T: 30, I: 20, Ag: 10, Dex: 30, Int: 20, WP: 40, Fel: 10 },
     },
     {
-      name: species[3],
-      species: 3,
+      name: speciesWithRegion[c.HIGH_ELF_DEFAULT],
+      species: c.HIGH_ELF_DEFAULT,
       expected: { WS: 30, BS: 30, S: 20, T: 20, I: 40, Ag: 30, Dex: 30, Int: 30, WP: 30, Fel: 20 },
     },
     {
-      name: species[4],
-      species: 4,
+      name: speciesWithRegion[c.WOOD_ELF_DEFAULT],
+      species: c.WOOD_ELF_DEFAULT,
       expected: { WS: 30, BS: 30, S: 20, T: 20, I: 40, Ag: 30, Dex: 30, Int: 30, WP: 30, Fel: 20 },
     },
     {
-      name: species[5],
-      species: 5,
+      name: speciesWithRegion[c.GNOME_DEFAULT],
+      species: c.GNOME_DEFAULT,
       expected: { WS: 20, BS: 10, S: 10, T: 15, I: 30, Ag: 30, Dex: 30, Int: 30, WP: 40, Fel: 15 },
+    },
+    {
+      name: speciesWithRegion[c.OGRE_DEFAULT],
+      species: c.OGRE_DEFAULT,
+      expected: { WS: 20, BS: 10, S: 35, T: 35, I: 0, Ag: 15, Dex: 10, Int: 10, WP: 20, Fel: 10 },
     },
   ])("when species is $name", (t) => {
     expect(getAttributes(t.species)).toEqual(t.expected);
