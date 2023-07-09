f39a695f02580a30de4948aa2ecb583b63f6e1c9
jacek <jacek.miloszewski@protonmail.com>
2023-07-09 13:34:10 +0100

    chore: clean-up of unused stuff

--- src/api/azure_wrapper.py
+++
@@ -1,9 +0,0 @@
-import azure.functions as func
-
-from . import create_app
-
-app = create_app()
-
-
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
