4d4a7059777e0eb7df96a9176fb9bd036d3f3fd5
jacek <jacek.miloszewski@protonmail.com>
2023-12-12 23:13:42 +0000

    refactored register

---
+++ src/frontend/src/components/FormStringInput.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    title: string;
+    isValid: boolean;
+    invalidMsg: string;
+    modelValue: string;
+  }>(),
+  {
+    title: "",
+    isValid: true,
+    invalidMsg: "",
+  },
+);
+
+const emit = defineEmits<{
+  (e: "update:modelValue", modelValue: string): void;
+}>();
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit("update:modelValue", value);
+  },
+});
+</script>
+
+<template>
+  <div class="w-full">
+    <p>{{ title }}</p>
+    <input
+      class="border-2 border-neutral-200 rounded w-full h-10 px-2 mt-1 focus:border-neutral-700 transition-colors duration-200"
+      v-model="value"
+    />
+    <p class="text-sm text-red-600" :class="[isValid ? 'hidden' : '']">{{ invalidMsg }}</p>
+  </div>
+</template>
