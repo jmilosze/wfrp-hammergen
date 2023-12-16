8cff351811f7c412721f86286d3d120926b408d7
jacek <jacek.miloszewski@protonmail.com>
2023-12-16 23:08:51 +0000

    feat: improved alerts

--- src/frontend/src/views/User/Register.vue
+++ src/frontend/src/views/User/Register.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import { Ref, ref } from "vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { useUserValidator } from "../../composables/userValidators.ts";
 import Header from "../../components/PageHeader.vue";
 import SubmitButton from "../../components/SubmitButton.vue";
 import FormStringInput from "../../components/FormStringInput.vue";
 import { anonRequest } from "../../services/auth";
+import Alert from "../../components/Alert.vue";
 
 const validatorOn = ref(false);
 const email = ref("");
 const password = ref("");
 const retypedPassword = ref("");
-const errors: Ref<string[]> = ref([]);
+const errors = ref("");
 const registering = ref(false);
 const registrationSuccessful = ref(false);
 
@@ -29,32 +30,31 @@ function onRegistrationSuccessful() {
 
   setTimeout(() => {
     router.push({ name: "home" });
-  }, 1000);
+  }, 1500);
 }
 
 function onRegistrationFailed(error: any) {
   if ("response" in error) {
     if (error.response && error.response.status === 409) {
-      errors.value.push("User with this email already exists.");
+      errors.value = "User with this email already exists.";
+      return;
     } else if (
       error.response &&
       error.response.status === 400 &&
       error.response.data.details === "captcha verification error"
     ) {
-      errors.value.push("We suspect you might be a robot. Please try again.");
-    } else {
-      errors.value.push("Server error.");
+      errors.value = "We suspect you might be a robot. Please try again.";
+      return;
     }
-  } else {
-    errors.value.push("Server error.");
   }
+  errors.value = "Server error.";
 }
 
 async function submitForm() {
   validatorOn.value = true;
   registering.value = false;
   registrationSuccessful.value = false;
-  errors.value = [];
+  errors.value = "";
   if (!validEmail.value || !validPassword.value || !passwordMatch.value) {
     return;
   }
@@ -84,43 +84,38 @@ async function submitForm() {
         but if you do, the password reset functionality will not work.
       </p>
     </Header>
-    <div class="flex lg:justify-start flex-col lg:flex-row">
-      <div class="flex flex-col md:flex-row">
-        <div class="pt-2 md:w-96">
-          <div class="flex flex-col items-start">
-            <FormStringInput
-              type="text"
-              class="mt-3"
-              v-model="email"
-              title="Email"
-              :invalidMsg="invalidEmailMsg"
-              :isValid="!validatorOn ? true : validEmail"
-            />
-            <FormStringInput
-              type="password"
-              class="mt-3"
-              v-model="password"
-              title="Password"
-              :invalidMsg="invalidPasswordMsg"
-              :isValid="!validatorOn ? true : validPassword"
-            />
-            <FormStringInput
-              type="password"
-              class="mt-3"
-              v-model="retypedPassword"
-              title="Confirm Password"
-              :invalidMsg="passwordDoNotMatchMsg"
-              :isValid="!validatorOn ? true : passwordMatch"
-            />
-          </div>
-          <SubmitButton class="mt-5" @click="submitForm" :processing="registering">Register</SubmitButton>
-        </div>
+    <div class="pt-2 md:w-96">
+      <Alert class="mt-3" alertType="red" :visible="errors !== ''">{{ errors }}</Alert>
+      <Alert class="mt-3" alertType="green" :visible="registrationSuccessful"
+        >Registration successful, redirecting to login...</Alert
+      >
+      <div class="flex flex-col items-start">
+        <FormStringInput
+          type="text"
+          class="mt-3"
+          v-model="email"
+          title="Email"
+          :invalidMsg="invalidEmailMsg"
+          :isValid="!validatorOn ? true : validEmail"
+        />
+        <FormStringInput
+          type="password"
+          class="mt-3"
+          v-model="password"
+          title="Password"
+          :invalidMsg="invalidPasswordMsg"
+          :isValid="!validatorOn ? true : validPassword"
+        />
+        <FormStringInput
+          type="password"
+          class="mt-3"
+          v-model="retypedPassword"
+          title="Confirm Password"
+          :invalidMsg="passwordDoNotMatchMsg"
+          :isValid="!validatorOn ? true : passwordMatch"
+        />
       </div>
-      <div class="pt-5 lg:pl-5">
-        <ul>
-          <li v-for="error in errors" :key="error" class="text-red-600">{{ error }}</li>
-        </ul>
-      </div>
+      <SubmitButton class="mt-3" @click="submitForm" :processing="registering">Register</SubmitButton>
     </div>
   </div>
 </template>
