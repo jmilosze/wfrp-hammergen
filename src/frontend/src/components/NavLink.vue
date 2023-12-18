6c4aa169d5fcaac7751bd8a5ec0fbde544c18669
jacek <jacek.miloszewski@protonmail.com>
2023-12-18 19:39:41 +0000

    ghf

--- src/frontend/src/components/NavLink.vue
+++ src/frontend/src/components/NavLink.vue
@@ -3,12 +3,14 @@ import { useRoute } from "vue-router";
 import { computed } from "vue";
 
 const SIDE_VARIANT = {
-  static: ["block hover:bg-neutral-700 hover:text-amber-300 py-1 px-2 rounded"],
-  hover: ["font-bold"],
+  static: ["block", "hover:bg-neutral-700", "hover:text-amber-300", "py-1", "px-2", "rounded"],
+  unselected: [],
+  selected: ["font-bold"],
 };
 const TOP_VARIANT = {
-  static: ["block text-amber-300 hover:bg-neutral-800 p-3 rounded"],
-  hover: ["text-amber-100"],
+  static: ["block", "hover:bg-neutral-800", "p-3", "rounded"],
+  unselected: ["text-amber-300"],
+  selected: ["text-amber-100"],
 };
 
 const props = defineProps<{ routeName?: string; href?: string; variant: "top" | "side" }>();
@@ -18,14 +20,14 @@ const route = useRoute();
 const style = computed(() => {
   if (props.variant == "top") {
     if (route.name == props.routeName && route.name != "placeholder") {
-      return TOP_VARIANT.static.concat(TOP_VARIANT.hover);
+      return TOP_VARIANT.static.concat(TOP_VARIANT.selected);
     }
-    return TOP_VARIANT.static;
+    return TOP_VARIANT.static.concat(TOP_VARIANT.unselected);
   } else {
     if (route.name == props.routeName && route.name != "placeholder") {
-      return SIDE_VARIANT.static.concat(SIDE_VARIANT.hover);
+      return SIDE_VARIANT.static.concat(SIDE_VARIANT.selected);
     }
-    return SIDE_VARIANT.static;
+    return SIDE_VARIANT.static.concat(SIDE_VARIANT.unselected);
   }
 });
 </script>
