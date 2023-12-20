adb2215d3425fc87bfc3a335666c972895bd07fe
jacek <jacek.miloszewski@protonmail.com>
2023-12-20 17:54:37 +0000

    fix: changed 'on' to workflow_dispatch

--- src/frontend/src/utils/submission.ts
+++ src/frontend/src/utils/submission.ts
@@ -1,4 +1,24 @@
-export type SubmissionState = {
+export class SubmissionState {
   status: "notStarted" | "inProgress" | "success" | "failure";
   message: string;
-};
+
+  constructor() {
+    this.status = "notStarted";
+    this.message = "";
+  }
+
+  reset() {
+    this.status = "notStarted";
+    this.message = "";
+  }
+
+  setSuccess(message: string) {
+    this.status = "success";
+    this.message = message;
+  }
+
+  setFailure(message: string) {
+    this.status = "failure";
+    this.message = message;
+  }
+}
