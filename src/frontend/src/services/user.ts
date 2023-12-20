a70bf31ac9d873e05be35b9c6ad65643e6103106
jacek <jacek.miloszewski@protonmail.com>
2023-12-20 19:16:18 +0000

    zxc

---
+++ src/frontend/src/services/user.ts
@@ -0,0 +1,40 @@
+const emailRegex =
+  /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/;
+const pwdRegex = /^.{5,30}$/;
+
+export class User {
+  email: string;
+  currentPassword: string;
+  password: string;
+  retypedPassword: string;
+
+  constructor() {
+    this.email = "";
+    this.currentPassword = "";
+    this.password = "";
+    this.retypedPassword = "";
+  }
+
+  reset() {
+    this.email = "";
+    this.currentPassword = "";
+    this.password = "";
+    this.retypedPassword = "";
+  }
+
+  validateEmail() {
+    return emailRegex.test(this.email);
+  }
+
+  validatePassword() {
+    return pwdRegex.test(this.password);
+  }
+
+  passwordMatch() {
+    return this.password == this.retypedPassword;
+  }
+}
+
+export const invalidEmailMsg = "Email address is invalid.";
+export const invalidPasswordMsg = "Password has to have between 5 and 30 characters.";
+export const passwordDoNotMatchMsg = "Passwords do not match.";
