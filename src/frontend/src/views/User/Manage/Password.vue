0e76f504b88e962c36f7045e0b821a961bc260f5
jacek <jacek.miloszewski@protonmail.com>
2023-12-20 22:53:07 +0000

    zxc

--- src/frontend/src/views/User/Manage/Password.vue
+++ src/frontend/src/views/User/Manage/Password.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
-<template><div>Password section</div></template>
+<template>
+  <div class="mt-5 pb-2 border-b-2 border-neutral-200">
+    <div class="text-xl">Change password</div>
+  </div>
+</template>
 
 <style scoped></style>
