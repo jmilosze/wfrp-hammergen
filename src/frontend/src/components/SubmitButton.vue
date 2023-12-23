da35e1175ad3da4384aa4f0b68f6976214a5a510
jacek <jacek.miloszewski@protonmail.com>
2023-12-23 19:57:00 +0000

    zxc

--- src/frontend/src/components/SubmitButton.vue
+++ src/frontend/src/components/SubmitButton.vue
@@ -8,9 +8,9 @@ const props = defineProps<{
 
 function setClass() {
   if (props.variant && props.variant == "danger") {
-    return "bg-red-600 hover:bg-red-800 rounded text-neutral-50 px-3 py-2 active:outline outline-2 outline-red-400";
+    return "bg-red-600 hover:bg-red-800 rounded text-neutral-50 px-3 py-2 active:outline outline-2 outline-red-400 select-none";
   } else {
-    return "bg-neutral-600 hover:bg-neutral-800 rounded text-neutral-50 px-3 py-2 active:outline outline-2 outline-neutral-400";
+    return "bg-neutral-600 hover:bg-neutral-800 rounded text-neutral-50 px-3 py-2 active:outline outline-2 outline-neutral-400 select-none";
   }
 }
 </script>
