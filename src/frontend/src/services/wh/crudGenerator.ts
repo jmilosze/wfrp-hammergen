aa51b6c1bbe0cb2c6a237323937a8a41a4cf82ff
jacek <jacek.miloszewski@protonmail.com>
2023-12-31 20:29:42 +0000

    zxc

--- src/frontend/src/services/wh/crudGenerator.ts
+++ src/frontend/src/services/wh/crudGenerator.ts
@@ -1,53 +1,53 @@
 import { AxiosInstance, AxiosResponse } from "axios";
-import { Wh } from "./common.ts";
+import { Wh, WhApiResponse } from "./common.ts";
 
-export function getElementFunc<WhApi extends Wh, WhModel extends Wh>(
+export function getElementFunc<WhApi, WhModel extends Wh>(
   apiBasePath: string,
   axiosInstance: AxiosInstance,
-  convertApiToModelData: (whApi: WhApi) => WhModel,
+  apiResponseToModel: (whApiResp: WhApiResponse<WhApi>) => WhModel,
 ) {
   return async (id: string) => {
     const serverResp = await axiosInstance.get(`${apiBasePath}/${id}`);
-    return convertApiToModelData((serverResp as AxiosResponse<{ data: WhApi }, any>).data.data);
+    return apiResponseToModel((serverResp as AxiosResponse<{ data: WhApiResponse<WhApi> }, any>).data.data);
   };
 }
 
-export function listElementsFunc<WhApi extends Wh, WhModel extends Wh>(
+export function listElementsFunc<WhApi, WhModel extends Wh>(
   apiBasePath: string,
   axiosInstance: AxiosInstance,
-  convertApiToModelData: (whApi: WhApi) => WhModel,
+  apiResponseToModel: (whApiResp: WhApiResponse<WhApi>) => WhModel,
 ) {
   return async () => {
     const serverResp = await axiosInstance.get(`${apiBasePath}`);
 
-    return (serverResp as AxiosResponse<{ data: WhApi[] }, any>).data.data.map(convertApiToModelData);
+    return (serverResp as AxiosResponse<{ data: WhApiResponse<WhApi>[] }, any>).data.data.map(apiResponseToModel);
   };
 }
 
-export function createElementFunc<WhApi extends Wh, WhModel extends Wh>(
+export function createElementFunc<WhApi, WhModel extends Wh>(
   apiBasePath: string,
   axiosInstance: AxiosInstance,
   convertModelToApiData: (wh: WhModel) => WhApi,
 ) {
   return async (wh: WhModel) => {
     const serverResp = await axiosInstance.post(`${apiBasePath}`, convertModelToApiData(wh));
-    return (serverResp as AxiosResponse<{ data: WhApi }, any>).data.data;
+    return (serverResp as AxiosResponse<{ data: WhApiResponse<WhApi> }, any>).data.data;
   };
 }
 
-export function updateElementFunc<WhApi extends Wh, WhModel extends Wh>(
+export function updateElementFunc<WhApi, WhModel extends Wh>(
   apiBasePath: string,
   axiosInstance: AxiosInstance,
   convertModelToApiData: (wh: WhModel) => WhApi,
 ) {
   return async (wh: WhModel) => {
     const serverResp = await axiosInstance.put(`${apiBasePath}/${wh.id}`, convertModelToApiData(wh));
-    return (serverResp as AxiosResponse<{ data: WhApi }, any>).data.data;
+    return (serverResp as AxiosResponse<{ data: WhApiResponse<WhApi> }, any>).data.data;
   };
 }
 
 export function deleteElementFunc(apiBasePath: string, axiosInstance: AxiosInstance) {
-  return async (id) => {
+  return async (id: string) => {
     await axiosInstance.delete(`${apiBasePath}/${id}`);
   };
 }
