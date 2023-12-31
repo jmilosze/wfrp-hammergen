669c0b295a89df159cd8287dd86a45c47bfe56cb
jacek <jacek.miloszewski@protonmail.com>
2023-12-31 20:29:42 +0000

    zxc

--- src/frontend/src/services/wh/common.ts
+++ src/frontend/src/services/wh/common.ts
@@ -1,7 +1,29 @@
 export interface Wh {
   id: string;
   canEdit: boolean;
+}
+
+export interface WhApiResponse<WhApi> {
+  id: string;
+  canEdit: boolean;
   ownerId: string;
+  object: WhApi;
 }
 
 export type WhSource = Record<string, string>;
+
+export function defaultSource(): WhSource {
+  return { 0: "" };
+}
+export function compareWhSources(whSource1: WhSource, whSource2: WhSource) {
+  if (Object.keys(whSource1).length != Object.keys(whSource2).length) {
+    return false;
+  }
+
+  for (const [key, val] of Object.entries(whSource1)) {
+    if (whSource2[key] != val) {
+      return false;
+    }
+  }
+  return true;
+}
--- src/frontend/src/services/wh/spell.ts
+++ src/frontend/src/services/wh/spell.ts
@@ -1,4 +1,4 @@
-import { Wh, WhSource } from "./common.ts";
+import { compareWhSources, defaultSource, Wh, WhApiResponse, WhSource } from "./common.ts";
 import {
   createElementFunc,
   deleteElementFunc,
@@ -10,44 +10,68 @@ import { AxiosInstance } from "axios";
 
 const API_BASE_PATH = "/api/wh/spell";
 
-interface WhSpellApi extends Wh {
-  object: {
-    name: string;
-    description: string;
-    cn: number;
-    range: string;
-    shared: boolean;
-    target: string;
-    source: WhSource;
-  };
-}
-
-export interface WhSpell extends Wh {
+interface WhSpellApi {
   name: string;
   description: string;
   cn: number;
   range: string;
+  duration: string;
   shared: boolean;
   target: string;
   source: WhSource;
 }
 
-function convertApiToModelData(whSpellApi: WhSpellApi): WhSpell {
-  return {
-    id: whSpellApi.id,
-    canEdit: whSpellApi.canEdit,
-    name: whSpellApi.object.name,
-    cn: whSpellApi.object.cn,
-    range: whSpellApi.object.range,
-    target: whSpellApi.object.target,
-    duration: whSpellApi.object.duration,
-    description: whSpellApi.object.description,
-    shared: whSpellApi.object.shared,
-    source: whSpellApi.object.source,
-  };
+export class WhSpell implements Wh {
+  id: string;
+  canEdit: boolean;
+  name: string;
+  description: string;
+  cn: number;
+  range: string;
+  duration: string;
+  shared: boolean;
+  target: string;
+  source: WhSource;
+
+  constructor() {
+    this.id = "";
+    this.name = "name";
+    this.cn = 0;
+    this.range = "";
+    this.target = "";
+    this.duration = "";
+    this.description = "";
+    this.canEdit = false;
+    this.shared = false;
+    this.source = {};
+  }
+
+  newSpell(canEdit: boolean) {
+    const spell = new WhSpell();
+    spell.canEdit = canEdit;
+    spell.shared = true;
+    spell.source = defaultSource();
+    return spell;
+  }
 }
 
-function convertModelToApiData(whSpell: WhSpell): WhSpellApi {
+function apiResponseToModel(whSpellApi: WhApiResponse<WhSpellApi>): WhSpell {
+  const spell = new WhSpell();
+  spell.id = whSpellApi.id;
+  spell.canEdit = whSpellApi.canEdit;
+  spell.name = whSpellApi.object.name;
+  spell.cn = whSpellApi.object.cn;
+  spell.range = whSpellApi.object.range;
+  spell.target = whSpellApi.object.target;
+  spell.duration = whSpellApi.object.duration;
+  spell.description = whSpellApi.object.description;
+  spell.shared = whSpellApi.object.shared;
+  spell.source = whSpellApi.object.source;
+
+  return spell;
+}
+
+function modelToApi(whSpell: WhSpell): WhSpellApi {
   return {
     name: whSpell.name,
     cn: whSpell.cn,
@@ -60,12 +84,34 @@ function convertModelToApiData(whSpell: WhSpell): WhSpellApi {
   };
 }
 
-class SpellApi {
+export class SpellApi {
+  getElement: (id: string) => Promise<WhSpell>;
+  listElements: (id: string) => Promise<WhSpell[]>;
+  createElement: (wh: WhSpell) => Promise<WhApiResponse<WhSpellApi>>;
+  updateElement: (wh: WhSpell) => Promise<WhApiResponse<WhSpellApi>>;
+  deleteElement: (id: string) => Promise<void>;
+
   constructor(axiosInstance: AxiosInstance) {
-    this.getElement = getElementFunc(API_BASE_PATH, axiosInstance, convertApiToModelData);
-    this.listElements = listElementsFunc(API_BASE_PATH, axiosInstance, convertApiToModelData);
-    this.createElement = createElementFunc(API_BASE_APTH, axiosInstance, convertModelToApiData);
-    this.updateElement = updateElementFunc(API_BASE_APTH, axiosInstance, convertModelToApiData);
-    this.deleteElement = deleteElementFunc(API_BASE_APTH, axiosInstance);
+    this.getElement = getElementFunc(API_BASE_PATH, axiosInstance, apiResponseToModel);
+    this.listElements = listElementsFunc(API_BASE_PATH, axiosInstance, apiResponseToModel);
+    this.createElement = createElementFunc(API_BASE_PATH, axiosInstance, modelToApi);
+    this.updateElement = updateElementFunc(API_BASE_PATH, axiosInstance, modelToApi);
+    this.deleteElement = deleteElementFunc(API_BASE_PATH, axiosInstance);
   }
 }
+
+export const compareSpell = (spell1: WhSpell, spell2: WhSpell) => {
+  if (
+    spell1.name != spell2.name ||
+    spell1.cn != spell2.cn ||
+    spell1.range != spell2.range ||
+    spell1.target != spell2.target ||
+    spell1.duration != spell2.duration ||
+    spell1.description != spell2.description ||
+    spell1.shared != spell2.shared
+  ) {
+    return false;
+  }
+
+  return compareWhSources(spell1.source, spell2.source);
+};
