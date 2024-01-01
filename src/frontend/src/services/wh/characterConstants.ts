6ceaf43233f3afa8830c9026f7d6e7efa2ae9b01
jacek <jacek.miloszewski@protonmail.com>
2024-01-01 10:52:31 +0000

    zxc

---
+++ src/frontend/src/services/wh/characterConstants.ts
@@ -0,0 +1,82 @@
+export const HUMAN_DEFAULT = "0000";
+export const HUMAN_REIKLAND = "0001"; // WFRP p. 25
+export const HUMAN_ALTDORF_SOUTH_BANK = "0002"; // Archives III p. 83
+export const HUMAN_ALTDORF_EASTEND = "0003"; // Archives III p. 83
+export const HUMAN_ALTDORF_HEXXERBEZRIK = "0004"; // Archives III p. 84
+export const HUMAN_ALTDORF_DOCKLANDS = "0005"; // Archives III p. 84
+export const HUMAN_MIDDENHEIM = "0006"; // Middenheim p. 151
+export const HUMAN_MIDDENLAND = "0007"; // Middenheim p. 152, Salzenmund p. 142
+export const HUMAN_NORDLAND = "0008"; // Middenheim p. 153
+export const HUMAN_SALZENMUND = "0009"; // Salzenmund p. 142
+export const HUMAN_TILEA = "0010"; // Up in Arms p. 55
+export const HUMAN_NORSE_BJORNLING = "0011"; // Sea of Claws p. 56
+export const HUMAN_NORSE_SARL = "0012"; // Sea of Claws p. 56
+export const HUMAN_NORSE_SKAELING = "0013"; // Sea of Claws p. 56
+export const HALFLING_DEFAULT = "0100"; // WFRP p. 26
+export const HALFLING_ASHFIELD = "0101"; // Archives I p. 32
+export const HALFLING_BRAMBLEDOWN = "0102"; // Archives I p. 32
+export const HALFLING_BRANDYSNAP = "0103"; // Archives I p. 32
+export const HALFLING_HAYFOOT = "0104"; // Archives I p. 32
+export const HALFLING_HOLLYFOOT = "0105"; // Archives I p. 32
+export const HALFLING_HAYFOOT_HOLLYFOOT = "0106"; // Archives I p. 32
+export const HALFLING_LOSTPOCKETS = "0107"; // Archives I p. 32
+export const HALFLING_LOWHAVEN = "0108"; // Archives I p. 32
+export const HALFLING_RUMSTER = "0109"; // Archives I p. 32
+export const HALFLING_SKELFSIDER = "0110"; // Archives I p. 32
+export const HALFLING_THORNCOBBLE = "0111"; // Archives I p. 32
+export const HALFLING_TUMBLEBERRY = "0112"; // Archives I p. 32
+export const DWARF_DEFAULT = "0200"; // WFRP p. 25
+export const DWARF_ALTDORF = "0201"; // Archives III p. 83
+export const DWARF_CRAGFORGE_CLAN = "0202"; // Salzenmund p. 142
+export const DWARF_GRUMSSON_CLAN = "0203"; // Salzenmund p. 142
+export const DWARF_NORSE = "0204"; // Sea of Claws p. 41
+export const HIGH_ELF_DEFAULT = "0300"; // WFRP p. 27
+export const WOOD_ELF_DEFAULT = "0400"; // WFRP p. 28
+export const GNOME_DEFAULT = "0500"; // Rough Nights and Hard Days p. 86
+export const OGRE_DEFAULT = "0600"; // Archives II p. 18
+
+export const HUMAN_LIST = [
+  HUMAN_DEFAULT,
+  HUMAN_REIKLAND,
+  HUMAN_ALTDORF_SOUTH_BANK,
+  HUMAN_ALTDORF_EASTEND,
+  HUMAN_ALTDORF_HEXXERBEZRIK,
+  HUMAN_ALTDORF_DOCKLANDS,
+  HUMAN_MIDDENHEIM,
+  HUMAN_MIDDENLAND,
+  HUMAN_NORDLAND,
+  HUMAN_SALZENMUND,
+  HUMAN_TILEA,
+  HUMAN_NORSE_BJORNLING,
+  HUMAN_NORSE_SARL,
+  HUMAN_NORSE_SKAELING,
+];
+
+export const HALFLING_LIST = [
+  HALFLING_DEFAULT,
+  HALFLING_ASHFIELD,
+  HALFLING_BRAMBLEDOWN,
+  HALFLING_BRANDYSNAP,
+  HALFLING_HAYFOOT,
+  HALFLING_HOLLYFOOT,
+  HALFLING_HAYFOOT_HOLLYFOOT,
+  HALFLING_LOSTPOCKETS,
+  HALFLING_LOWHAVEN,
+  HALFLING_RUMSTER,
+  HALFLING_SKELFSIDER,
+  HALFLING_THORNCOBBLE,
+  HALFLING_TUMBLEBERRY,
+];
+
+export const DWARF_LIST = [DWARF_DEFAULT, DWARF_ALTDORF, DWARF_CRAGFORGE_CLAN, DWARF_GRUMSSON_CLAN, DWARF_NORSE];
+
+export const HIGH_ELF_LIST = [HIGH_ELF_DEFAULT];
+
+export const WOOD_ELF_LIST = [WOOD_ELF_DEFAULT];
+
+export const GNOME_LIST = [GNOME_DEFAULT];
+
+export const OGRE_LIST = [OGRE_DEFAULT];
+
+export const MALE = 0;
+export const FEMALE = 1;
