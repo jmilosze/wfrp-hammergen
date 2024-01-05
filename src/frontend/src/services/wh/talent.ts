593098a16e549bf630493ba3d14e61e42467c7e1
jacek <jacek.miloszewski@protonmail.com>
2024-01-05 21:56:42 +0000

    zxc

--- src/frontend/src/services/wh/talent.ts
+++ src/frontend/src/services/wh/talent.ts
@@ -1,6 +1,16 @@
 import { CharacterModifiers } from "./characterModifiers.ts";
-import { Source } from "./source.ts";
-import { WhProperty } from "./crudGenerator.ts";
+import { copySource, Source, sourcesAreEqual } from "./source.ts";
+import {
+  ApiResponse,
+  createElementFunc,
+  deleteElementFunc,
+  getElementFunc,
+  listElementsFunc,
+  updateElementFunc,
+  WhProperty,
+} from "./crudGenerator.ts";
+import { compareArrayIgnoreOrder } from "../../utils/arrayUtils.ts";
+import { AxiosInstance } from "axios";
 
 enum TalentAttribute {
   None = 0,
@@ -17,9 +27,9 @@ enum TalentAttribute {
   Various,
 }
 
-// const API_BASE_PATH = "/api/wh/talent";
+const API_BASE_PATH = "/api/wh/talent";
 
-export interface MutationApiData {
+export interface TalentApiData {
   name: string;
   description: string;
   tests: string;
@@ -39,7 +49,7 @@ export class Talent implements WhProperty {
   description: string;
   tests: string;
   maxRank: number;
-  maxRankAtt: number;
+  attribute: number;
   isGroup: boolean;
   group: string[];
   modifiers: CharacterModifiers;
@@ -53,7 +63,7 @@ export class Talent implements WhProperty {
     description = "",
     tests = "",
     maxRank = 0,
-    maxRankAtt = TalentAttribute.None.valueOf(),
+    attribute = TalentAttribute.None.valueOf(),
     isGroup = false,
     group = [],
     modifiers = new CharacterModifiers(),
@@ -66,11 +76,108 @@ export class Talent implements WhProperty {
     this.description = description;
     this.tests = tests;
     this.maxRank = maxRank;
-    this.maxRankAtt = maxRankAtt;
+    this.attribute = attribute;
     this.isGroup = isGroup;
     this.group = JSON.parse(JSON.stringify(group));
     this.modifiers = modifiers;
     this.shared = shared;
     this.source = source;
+  }
+
+  copy() {
+    return new Talent({
+      id: this.id,
+      canEdit: this.canEdit,
+      name: this.name,
+      description: this.description,
+      tests: this.tests,
+      maxRank: this.maxRank,
+      attribute: this.attribute,
+      isGroup: this.isGroup,
+      group: JSON.parse(JSON.stringify(this.group)),
+      modifiers: this.modifiers.copy(),
+      shared: this.shared,
+      source: copySource(this.source),
+    });
+  }
+
+  isEqualTo(otherTalent: Talent): boolean {
+    if (
+      this.id !== otherTalent.id ||
+      this.canEdit !== otherTalent.canEdit ||
+      this.name !== otherTalent.name ||
+      this.description !== otherTalent.description ||
+      this.isGroup !== otherTalent.isGroup ||
+      this.shared !== otherTalent.shared ||
+      !sourcesAreEqual(this.source, otherTalent.source)
+    ) {
+      return false;
+    }
+
+    if (this.isGroup) {
+      return true;
+    }
+
+    if (
+      this.tests !== otherTalent.tests ||
+      this.maxRank !== otherTalent.maxRank ||
+      this.attribute !== otherTalent.attribute
+    ) {
+      return false;
+    }
+
+    if (!compareArrayIgnoreOrder(this.group, otherTalent.group)) {
+      return false;
+    }
+
+    return this.modifiers.isEqualTo(otherTalent.modifiers);
+  }
+}
+
+export function apiResponseToModel(talentApi: ApiResponse<TalentApiData>): Talent {
+  return new Talent({
+    id: talentApi.id,
+    canEdit: talentApi.canEdit,
+    name: talentApi.object.name,
+    description: talentApi.object.description,
+    tests: talentApi.object.tests,
+    maxRank: talentApi.object.maxRank,
+    attribute: talentApi.object.attribute,
+    isGroup: talentApi.object.isGroup,
+    group: JSON.parse(JSON.stringify(talentApi.object.group)),
+    modifiers: new CharacterModifiers(talentApi.object.modifiers),
+    shared: talentApi.object.shared,
+    source: copySource(talentApi.object.source),
+  });
+}
+
+export function modelToApi(talent: Talent): TalentApiData {
+  return {
+    name: talent.name,
+    description: talent.description,
+    tests: talent.tests,
+    maxRank: talent.maxRank,
+    attribute: talent.attribute,
+    isGroup: talent.isGroup,
+    group: JSON.parse(JSON.stringify(talent.group)),
+    modifiers: talent.modifiers.copy(),
+    shared: talent.shared,
+    source: copySource(talent.source),
+  };
+}
+
+export class TalentApi {
+  getElement: (id: string) => Promise<Talent>;
+  listElements: (id: string) => Promise<Talent[]>;
+  createElement: (wh: Talent) => Promise<ApiResponse<TalentApiData>>;
+  updateElement: (wh: Talent) => Promise<ApiResponse<TalentApiData>>;
+  deleteElement: (id: string) => Promise<void>;
+
+  constructor(axiosInstance: AxiosInstance) {
+    this.getElement = getElementFunc(API_BASE_PATH, axiosInstance, apiResponseToModel);
+    this.listElements = listElementsFunc(API_BASE_PATH, axiosInstance, apiResponseToModel);
+    this.createElement = createElementFunc(API_BASE_PATH, axiosInstance, modelToApi);
+    this.updateElement = updateElementFunc(API_BASE_PATH, axiosInstance, modelToApi);
+    this.deleteElement = deleteElementFunc(API_BASE_PATH, axiosInstance);
   }
 }
---
+++ src/frontend/src/utils/arrayUtils.ts
@@ -0,0 +1,34 @@
+function defaultCompare<T>(x: T, y: T): number {
+  if (x === y) {
+    return 0;
+  } else {
+    return x < y ? -1 : 1;
+  }
+}
+
+export function compareIdNumber(x: { id: string; number: number }, y: { id: string; number: number }): number {
+  const xIdNumber = `${x.id}_${x.number}`;
+  const yIdNumber = `${y.id}_${y.number}`;
+
+  if (xIdNumber === yIdNumber) {
+    return 0;
+  } else {
+    return xIdNumber < yIdNumber ? -1 : 1;
+  }
+}
+
+export function compareArrayIgnoreOrder<T>(array1: T[], array2: T[], compareFunc = defaultCompare) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  const sortedArray1 = array1.concat().sort(compareFunc);
+  const sortedArray2 = array2.concat().sort(compareFunc);
+
+  for (let i = 0; i < sortedArray1.length; ++i) {
+    if (compareFunc(sortedArray1[i], sortedArray2[i]) !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
