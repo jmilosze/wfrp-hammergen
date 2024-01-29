3af52a52b06575b5897d33424c04d964284a7c73
jacek <jacek.miloszewski@protonmail.com>
2024-01-29 00:11:59 +0000

    zxc

---
+++ src/frontend/src/utils/randomUtils.ts
@@ -0,0 +1,20 @@
+export function selectRandom<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+export function diceRoll(sides: number, rolls: number): number {
+  let sumOfRolls = 0;
+  for (let i = 0; i < rolls; ++i) {
+    sumOfRolls += Math.floor(Math.random() * sides) + 1;
+  }
+  return sumOfRolls;
+}
+
+export function rollInTable<T>(sides: number, rolls: number, table: [T, number, number][]): T | undefined {
+  const roll = diceRoll(sides, rolls);
+  for (const element of table) {
+    if (roll >= element[1] && roll < element[2]) {
+      return element[0];
+    }
+  }
+}
