b0d0dae737b3c8298e494b3c50a8f419c5b30867
jacek <jacek.miloszewski@protonmail.com>
2024-02-01 22:58:49 +0000

    asd

--- src/frontend/src/services/wh/characterGeneration/talentGeneration.ts
+++ src/frontend/src/services/wh/characterGeneration/talentGeneration.ts
@@ -9,11 +9,12 @@ import {
   sumAttributes,
 } from "../attributes.ts";
 import { IdNumber } from "../common.ts";
+import { SelectRandomFn } from "../../../utils/randomUtils.ts";
 
-export function getTalentGroups(listOfTalents: Talent[]): Record<string, string[]> {
+export function getTalentGroups(listOfWhTalents: Talent[]): Record<string, string[]> {
   const resolvedGroups: Record<string, string[]> = {};
 
-  for (const talent of listOfTalents) {
+  for (const talent of listOfWhTalents) {
     if (talent.group.length > 0) {
       for (const group of talent.group) {
         if (group in resolvedGroups) {
@@ -29,23 +30,23 @@ export function getTalentGroups(listOfTalents: Talent[]): Record<string, string[
 
 export function getAllTalentsMaxRank(
   selectedTalents: IdNumber[],
-  listOfTalents: Talent[],
+  listOfWhTalents: Talent[],
   baseAtts: Attributes,
   advances: Attributes,
 ) {
-  const attributes = sumAttributes(baseAtts, advances, getTalentAtts(selectedTalents, listOfTalents));
+  const attributes = sumAttributes(baseAtts, advances, getTalentAtts(selectedTalents, listOfWhTalents));
 
   const talentsRank: Record<string, number> = {};
-  for (const talent of listOfTalents) {
+  for (const talent of listOfWhTalents) {
     talentsRank[talent.id] = getMaxRank(talent, attributes);
   }
   return talentsRank;
 }
 
-export function getTalentAtts(selectedTalents: IdNumber[], listOfTalents: Talent[]): Attributes {
+export function getTalentAtts(selectedTalents: IdNumber[], listOfWhTalents: Talent[]): Attributes {
   const attributes = getAttributes();
 
-  for (const talent of listOfTalents) {
+  for (const talent of listOfWhTalents) {
     for (const idNumber of selectedTalents) {
       if (talent.id === idNumber.id) {
         sumAttributes(attributes, multiplyAttributes(idNumber.number, copyAttributes(talent.modifiers.attributes)));
@@ -61,4 +62,28 @@ function getMaxRank(talent: Talent, atts: Attributes): number {
   } else {
     return talent.maxRank;
   }
+}
+
+function generateAvailableTalents(
+  talents: string[],
+  talentGroups: Record<string, string[]>,
+  selectRandomFn: SelectRandomFn,
+): string[] {
+  const talentGroupsCopy: Record<string, string[]> = JSON.parse(JSON.stringify(talentGroups));
+  const availTalents: string[] = [];
+
+  for (const talent of talents) {
+    if (talent in talentGroupsCopy) {
+      if (talentGroupsCopy[talent].length > 0) {
+        const newTalent = selectRandomFn(talentGroupsCopy[talent]);
+        const indexToRemove = talentGroupsCopy[talent].indexOf(newTalent);
+        talentGroupsCopy[talent].splice(indexToRemove, 1);
+        availTalents.push(newTalent);
+      }
+    } else {
+      availTalents.push(talent);
+    }
+  }
+
+  return [...new Set(availTalents)];
 }
