d56d2e452144d162d50fc4011c7d8ec7b091c00b
jacek <jacek.miloszewski@protonmail.com>
2024-02-06 20:55:38 +0000

    fix generateAdv

---
+++ src/frontend/src/components/TableWithSearch.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const props = defineProps<{
+  fields: { name: string; displayName: string }[];
+  items: { name: string; value: string | number }[];
+}>();
+
+const rows: Array<Array<string | number>> = [];
+
+for (const item of props.items) {
+  // const row: Array<string | number> = [];
+  // for (const field of props.fields) {
+  // }
+}
+</script>
+
+<template>
+  <div class="pt-2 overflow-x-auto select-none">
+    <div class="mt-3 bg-neutral-50 rounded-xl border border-neutral-200">
+      <table class="w-full">
+        <thead>
+          <tr>
+            <th v-for="field in fields" :key="field.name" class="border-b border-neutral-200 p-2">
+              {{ field.displayName }}
+            </th>
+          </tr>
+        </thead>
+        <!--    <tbody>-->
+        <!--      <tr v-for="row in rows" :key="row.id">-->
+        <!--        <td v-for="(value, columnName) in row" :key="columnName">{{ value }}</td>-->
+        <!--      </tr>-->
+        <!--    </tbody>-->
+      </table>
+      <div class="bg-neutral-50 rounded-b-xl h-5 w-full"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/frontend/src/views/Warhammer/ListPrayers.vue
+++ src/frontend/src/views/Warhammer/ListPrayers.vue
@@ -2,15 +2,26 @@
 import { useWhListUtils } from "../../composables/whListUtils.ts";
 import { PrayerApi } from "../../services/wh/prayer.ts";
 import { authRequest } from "../../services/auth.ts";
+import TableWithSearch from "../../components/TableWithSearch.vue";
+import Header from "../../components/PageHeader.vue";
 
 const whListUtils = useWhListUtils(new PrayerApi(authRequest));
+const columns = [
+  { name: "col1", displayName: "Column One" },
+  { name: "col2", displayName: "Column Two" },
+  { name: "col3", displayName: "Column Three" },
+  { name: "col4", displayName: "Column Four" },
+];
 
 await whListUtils.loadWhList();
-for (const [_, prayer] of whListUtils.whList.value.entries()) {
+for (const prayer of whListUtils.whList.value.values()) {
   console.log(JSON.stringify(prayer));
 }
 </script>
 
-<template></template>
+<template>
+  <Header title="Prayers"> </Header>
+  <TableWithSearch :fields="columns"></TableWithSearch>
+</template>
 
 <style scoped></style>
