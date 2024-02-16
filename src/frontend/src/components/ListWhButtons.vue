9db4a295534f039439151cb58d0915542f2dc9f7
jacek <jacek.miloszewski@protonmail.com>
2024-02-16 08:18:50 +0000

    fix generateAdv

--- src/frontend/src/components/ListWhButtons.vue
+++ src/frontend/src/components/ListWhButtons.vue
@@ -2,9 +2,9 @@
 
 <template>
   <button
-    class="bg-neutral-600 hover:bg-neutral-800 rounded text-neutral-50 px-2 py-2 active:outline outline-2 outline-neutral-400 select-none text-sm"
+    class="bg-neutral-600 hover:bg-neutral-800 rounded text-neutral-50 px-2 py-1 active:outline outline-2 outline-neutral-400 select-none text-sm"
   >
-    View/Edit
+    Edit
   </button>
 </template>
 
