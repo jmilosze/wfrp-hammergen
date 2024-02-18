892457f0b183e5a9cdd35c63cc048e20f8787da4
jacek <jacek.miloszewski@protonmail.com>
2024-02-18 19:42:03 +0000

    fix generateAdv

--- src/frontend/src/components/ModalWindow.vue
+++ src/frontend/src/components/ModalWindow.vue
@@ -11,7 +11,23 @@ const modal = useModal();
 <template>
   <Teleport v-if="id === modal.modalId.value" to="#modal">
     <div class="flex justify-center">
-      <div class="bg-white rounded-lg shadow p-3 m-5 w-fit" @click.stop>
+      <div class="bg-white rounded-lg shadow p-3 m-5 w-fit border border-neutral-800" @click.stop>
+        <div class="flex justify-between">
+          <button
+            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            @click="modal.hideModal()"
+          >
+            <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+          </button>
+        </div>
         <slot></slot>
       </div>
     </div>
