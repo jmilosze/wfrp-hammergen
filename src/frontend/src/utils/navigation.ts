54e6853fdab0860f1f210c0bc481d4bf98d65a56
jacek <jacek.miloszewski@protonmail.com>
2024-02-19 07:53:11 +0000

    fix generateAdv

--- src/frontend/src/utils/navigation.ts
+++ src/frontend/src/utils/navigation.ts
@@ -8,8 +8,8 @@ export function addParamsToPath(path: string, params: Record<string, string>) {
     }
   }
   if (encodedPairs.length > 0) {
-    history.pushState({}, "", path + "?" + encodedPairs.join("&"));
+    history.replaceState({}, "", path + "?" + encodedPairs.join("&"));
   } else {
-    history.pushState({}, "", path);
+    history.replaceState({}, "", path);
   }
 }
