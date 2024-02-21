efbf8fe09692e796f8812889b96d3c8db0362b71
jacek <jacek.miloszewski@protonmail.com>
2024-02-21 22:37:38 +0000

    fix generateAdv

--- src/frontend/src/utils/queryParams.ts
+++ src/frontend/src/utils/queryParams.ts
@@ -1,15 +1,11 @@
 import { LocationQueryRaw } from "vue-router";
 
-export type SimpleQuery = Record<string, string | number>;
+export type SimpleQuery = Record<string, string>;
 
 export function queryParamsFromRouterQuery(queryParams: SimpleQuery, routerQuery: LocationQueryRaw) {
   for (const key of Object.keys(queryParams)) {
     if (key in routerQuery) {
-      if (typeof queryParams[key] === "number") {
-        queryParams[key] = Number(routerQuery[key]);
-      } else {
-        queryParams[key] = String(routerQuery[key]);
-      }
+      queryParams[key] = String(routerQuery[key]);
     }
   }
 }
@@ -17,7 +13,7 @@ export function queryParamsFromRouterQuery(queryParams: SimpleQuery, routerQuery
 export function queryParamsToRouterQuery(queryParams: SimpleQuery): LocationQueryRaw {
   const newQuery = {} as LocationQueryRaw;
   for (const [key, value] of Object.entries(queryParams)) {
-    if (value !== "" && value !== -1) {
+    if (value !== "") {
       newQuery[key] = value;
     }
   }
