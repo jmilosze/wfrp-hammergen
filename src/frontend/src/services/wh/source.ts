8d517821552a200fafb0f3d22705374841abdff0
jacek <jacek.miloszewski@protonmail.com>
2024-02-22 22:17:35 +0000

    fix generateAdv

--- src/frontend/src/services/wh/source.ts
+++ src/frontend/src/services/wh/source.ts
@@ -31,10 +31,6 @@ export const source: Record<string, string> = {
 
 export type Source = Record<string, string>;
 
-export function defaultSource(): Source {
-  return { 0: "" };
-}
-
 export function copySource(source: Source) {
   return JSON.parse(JSON.stringify(source));
 }
