cd6a2db4b64905f885bb777d91261a9ab1eeefd6
jacek <jacek.miloszewski@protonmail.com>
2024-02-23 22:56:03 +0000

    fix generateAdv

--- src/frontend/src/components/TableWithSearch.vue
+++ src/frontend/src/components/TableWithSearch.vue
@@ -92,59 +92,62 @@ onUpdated(() => {
         class="border border-neutral-300 rounded h-10 px-2 focus:border-neutral-700 focus:border-2 grow"
       />
     </div>
-    <TablePagination
-      v-model="startRow"
-      :totalRows="searchedItems.length"
-      :rowsPerPage="rowsPerPage"
-      class="mt-3"
-      @update:modelValue="needToScroll = 'top'"
-    />
-    <div class="overflow-x-auto">
-      <div class="mt-3 bg-neutral-50 rounded-xl border border-neutral-300 min-w-fit">
-        <table v-if="isEqualOrGreater" class="w-full">
-          <thead>
-            <tr class="text-left">
-              <th v-for="field in fields" :key="field.name" class="border-b border-neutral-300 py-2 px-5">
-                {{ field.displayName }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in itemsOnPage" :key="item.id" class="bg-white hover:bg-neutral-200">
-              <td v-for="field in fields" :key="field.name" class="py-2 px-5 border-b border-neutral-300">
-                <slot :name="field.name" v-bind="item">{{ item[field.name] }}</slot>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <table v-else class="w-full">
-          <thead>
-            <tr class="text-left">
-              <th class="border-b border-neutral-300 py-2 px-5"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in itemsOnPage" :key="item.id" class="bg-white hover:bg-neutral-200">
-              <td class="text-sm">
-                <div v-for="field in fields" :key="field.name" class="py-2 px-5 border-b border-neutral-300">
-                  <span class="font-bold">{{ field.displayName }}: </span>
+    <div v-if="items.length > 0">
+      <TablePagination
+        v-model="startRow"
+        :totalRows="searchedItems.length"
+        :rowsPerPage="rowsPerPage"
+        class="mt-3"
+        @update:modelValue="needToScroll = 'top'"
+      />
+      <div class="overflow-x-auto">
+        <div class="mt-3 bg-neutral-50 rounded-xl border border-neutral-300 min-w-fit">
+          <table v-if="isEqualOrGreater" class="w-full">
+            <thead>
+              <tr class="text-left">
+                <th v-for="field in fields" :key="field.name" class="border-b border-neutral-300 py-2 px-5">
+                  {{ field.displayName }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in itemsOnPage" :key="item.id" class="bg-white hover:bg-neutral-200">
+                <td v-for="field in fields" :key="field.name" class="py-2 px-5 border-b border-neutral-300">
                   <slot :name="field.name" v-bind="item">{{ item[field.name] }}</slot>
-                </div>
-                <div class="border-b-4 border-neutral-400"></div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div class="bg-neutral-50 rounded-b-xl h-5 w-full"></div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <table v-else class="w-full">
+            <thead>
+              <tr class="text-left">
+                <th class="border-b border-neutral-300 py-2 px-5"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in itemsOnPage" :key="item.id" class="bg-white hover:bg-neutral-200">
+                <td class="text-sm">
+                  <div v-for="field in fields" :key="field.name" class="py-2 px-5 border-b border-neutral-300">
+                    <span class="font-bold">{{ field.displayName }}: </span>
+                    <slot :name="field.name" v-bind="item">{{ item[field.name] }}</slot>
+                  </div>
+                  <div class="border-b-4 border-neutral-400"></div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="bg-neutral-50 rounded-b-xl h-5 w-full"></div>
+        </div>
       </div>
+      <TablePagination
+        v-model="startRow"
+        :totalRows="searchedItems.length"
+        :rowsPerPage="rowsPerPage"
+        class="mt-3"
+        @update:modelValue="needToScroll = 'bottom'"
+      />
     </div>
-    <TablePagination
-      v-model="startRow"
-      :totalRows="searchedItems.length"
-      :rowsPerPage="rowsPerPage"
-      class="mt-3"
-      @update:modelValue="needToScroll = 'bottom'"
-    />
+    <div v-else class="mt-2">No results found.</div>
   </div>
 </template>
 
