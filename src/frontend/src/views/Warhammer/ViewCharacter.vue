0c532b1dee358f94f8fc214ab592d60564540455
jacek <jacek.miloszewski@protonmail.com>
2024-02-25 14:40:09 +0000

    fix generateAdv

--- src/frontend/src/views/Warhammer/ViewCharacter.vue
+++ src/frontend/src/views/Warhammer/ViewCharacter.vue
@@ -5,6 +5,8 @@ import { ref, Ref } from "vue";
 import { CharacterApi } from "../../services/wh/character.ts";
 import { authRequest } from "../../services/auth.ts";
 import { newCharacterFull } from "../../services/wh/characterFull.ts";
+import ActionButton from "../../components/ActionButton.vue";
+import { saveAs } from "file-saver";
 
 const props = defineProps<{
   id: string;
@@ -28,11 +30,24 @@ async function loadCharacter() {
     }
   }
 }
+
+function saveJson() {
+  const blob = new Blob([JSON.stringify(character.value, null, 2)], {
+    type: "text/plain;charset=utf-8",
+  });
+  saveAs(blob, `${character.value.name}.json`);
+}
 </script>
 
 <template>
   <Header title="Character details"> </Header>
-  <div>View character {{ character.name }}</div>
+  <div class="flex flex-wrap">
+    <ActionButton :size="'sm'" class="m-1">Download CSV</ActionButton>
+    <ActionButton :size="'sm'" class="m-1" @click="saveJson()">Download JSON</ActionButton>
+    <ActionButton :size="'sm'" class="m-1">Print</ActionButton>
+    <ActionButton v-if="character.canEdit" :size="'sm'" class="m-1">Edit</ActionButton>
+    <ActionButton :size="'sm'" class="m-1">Back to list</ActionButton>
+  </div>
 </template>
 
 <style scoped></style>
