f615d05e8845c755f38e8f8d9ac8b0aa8555de09
jacek <jacek.miloszewski@protonmail.com>
2024-03-13 19:41:33 +0000

    fix generateAdv

--- src/frontend/src/components/MultipleCheckboxInput.vue
+++ src/frontend/src/components/MultipleCheckboxInput.vue
@@ -1,26 +1,38 @@
 <script setup lang="ts" generic="T = string | number">
-const model = defineModel<T[]>();
-
 const props = defineProps<{
+  modelValue: T[];
   title?: string;
   disabled?: boolean;
   options: { text: string; value: T }[];
 }>();
 
+const emit = defineEmits<{
+  (e: "update:modelValue", modelValue: T[]): void;
+}>();
+
 function isSelected(value: T): boolean {
-  return !!model?.value?.includes(value);
+  return props.modelValue.includes(value);
 }
-// @change="handleChange"
-// handleChange(event: Event) {
-//   const target = event.target as HTMLInputElement;
-//   let newValue = [...this.modelValue];
-//   if (target.checked) {
-//     newValue.push(target.value);
-//   } else {
-//     newValue = newValue.filter((value) => value !== target.value);
-//   }
-//   this.$emit('update:modelValue', newValue);
-// },
+
+function handleChange(event: Event) {
+  const target = event.target as HTMLInputElement;
+  let newValue = [...props.modelValue];
+  let changed: T;
+
+  if (typeof props.options[0].value === "number") {
+    // due to this options have to have at least 1 element
+    changed = parseInt(target.id) as T;
+  } else {
+    changed = target.id as T;
+  }
+
+  if (target.checked) {
+    newValue.push(changed);
+  } else {
+    newValue = newValue.filter((value) => value !== changed);
+  }
+  emit("update:modelValue", newValue);
+}
 </script>
 
 <template>
@@ -28,10 +40,12 @@ function isSelected(value: T): boolean {
     <p v-if="title" class="mb-1">{{ title }}</p>
     <div v-for="option in options" :key="String(option.value)" class="inline-flex items-center">
       <input
+        :id="String(option.value)"
         type="checkbox"
         :checked="isSelected(option.value)"
         :disabled="disabled ? disabled : false"
         class="w-5 h-5 accent-neutral-600"
+        @change="handleChange"
       />
       <div class="ml-1 mr-3">{{ option.text }}</div>
     </div>
