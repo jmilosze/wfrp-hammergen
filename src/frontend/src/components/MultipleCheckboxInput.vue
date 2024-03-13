a3a69ffef061ffdc556301cf05e3992cef83b499
jacek <jacek.miloszewski@protonmail.com>
2024-03-13 08:23:09 +0000

    fix generateAdv

--- src/frontend/src/components/MultipleCheckboxInput.vue
+++ src/frontend/src/components/MultipleCheckboxInput.vue
@@ -6,13 +6,22 @@ const props = defineProps<{
   disabled?: boolean;
   options: { text: string; value: T }[];
 }>();
+
+function isSelected(value: T): boolean {
+  return !!model?.value?.includes(value);
+}
 </script>
 
 <template>
   <div>
     <p v-if="title" class="mb-1">{{ title }}</p>
     <div v-for="option in options" :key="String(option.value)" class="inline-flex items-center">
-      <input type="checkbox" :disabled="disabled ? disabled : false" class="w-5 h-5 accent-neutral-600" />
+      <input
+        type="checkbox"
+        :checked="isSelected(option.value)"
+        :disabled="disabled ? disabled : false"
+        class="w-5 h-5 accent-neutral-600"
+      />
       <div class="ml-1 mr-3">{{ option.text }}</div>
     </div>
   </div>
