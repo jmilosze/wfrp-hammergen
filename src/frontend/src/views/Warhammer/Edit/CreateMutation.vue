6374d1e43937b2d9f2fcf4ed9c5ca91e3bb90686
jacek <jacek.miloszewski@protonmail.com>
2024-03-14 19:18:26 +0000

    fix generateAdv

--- src/frontend/src/views/Warhammer/Edit/CreateMutation.vue
+++ src/frontend/src/views/Warhammer/Edit/CreateMutation.vue
@@ -2,7 +2,7 @@
 import { defaultSource } from "../../../services/wh/source.ts";
 import { useWhEdit } from "../../../composables/whEdit.ts";
 import { authRequest } from "../../../services/auth.ts";
-import { Mutation, MutationApi } from "../../../services/wh/mutation.ts";
+import { Mutation, MutationApi, mutationTypeList, printMutationType } from "../../../services/wh/mutation.ts";
 import { computed, ref } from "vue";
 import { useElSize } from "../../../composables/viewSize.ts";
 import { ViewSize } from "../../../utils/viewSize.ts";
@@ -12,6 +12,11 @@ import MultipleCheckboxInput from "../../../components/MultipleCheckboxInput.vue
 import FormInput from "../../../components/FormInput.vue";
 import SelectInput from "../../../components/SelectInput.vue";
 import FormTextarea from "../../../components/FormTextarea.vue";
+import { itemPropertyTypeList, printItemPropertyType } from "../../../services/wh/itemproperty.ts";
+import PublicPropertyBox from "../../../components/PublicPropertyBox.vue";
+import AfterSubmit from "../../../components/AfterSubmit.vue";
+import SourceTable from "../../../components/SourceTable.vue";
+import EditControls from "../../../components/EditControls.vue";
 
 const props = defineProps<{
   id: string;
@@ -45,6 +50,8 @@ const { isEqualOrGreater } = useElSize(ViewSize.md, contentContainerRef);
 
 const validName = computed(() => wh.value.validateName());
 const validDesc = computed(() => wh.value.validateDescription());
+
+const typeOptions = ref(mutationTypeList.map((x) => ({ text: printMutationType(x), value: x })));
 </script>
 
 <template>
@@ -62,6 +69,7 @@ const validDesc = computed(() => wh.value.validateDescription());
     <div class="my-3 flex-1">
       <div class="flex flex-col gap-4">
         <FormInput v-model="wh.name" title="Name" :validationStatus="validName" :disabled="!wh.canEdit" />
+        <SelectInput v-model="wh.type" :options="typeOptions" :disabled="!wh.canEdit" title="Type"></SelectInput>
       </div>
     </div>
     <div class="my-3 flex-1">
@@ -74,6 +82,41 @@ const validDesc = computed(() => wh.value.validateDescription());
         />
       </div>
     </div>
+  </div>
+
+  <div
+    ref="contentContainerRef"
+    class="justify-between text-left gap-4"
+    :class="[isEqualOrGreater ? 'flex' : 'flex-col']"
+  >
+    <div class="my-3 flex-1">
+      <SourceTable
+        :disabled="!wh.canEdit"
+        :initSources="initSources"
+        @selected="(e) => wh.updateSource(e)"
+      ></SourceTable>
+    </div>
+    <div class="my-3 flex-1">
+      <PublicPropertyBox v-model="wh.shared" propertyName="Quality/rune" :disabled="!wh.canEdit" />
+    </div>
+  </div>
+  <div class="mt-4">
+    <AfterSubmit
+      :visible="showSubmissionStatus"
+      :submissionState="submissionState"
+      class="w-fit"
+      @close="showSubmissionStatus = false"
+    />
+
+    <EditControls
+      :saving="submissionState.status === 'inProgress'"
+      list="properties"
+      :allowAddAnother="id === 'create'"
+      :confirmExit="hasChanged"
+      :submitForm="submitForm"
+      :resetForm="resetForm"
+      :readOnly="!wh.canEdit"
+    ></EditControls>
   </div>
 </template>
 
