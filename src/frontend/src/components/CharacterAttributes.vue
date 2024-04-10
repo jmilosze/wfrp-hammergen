c3a0c6ca61bd07ead7548ce2093f7eb62845b946
jacek <jacek.miloszewski@protonmail.com>
2024-04-10 08:42:20 +0100

    zxc

--- src/frontend/src/components/CharacterAttributes.vue
+++ src/frontend/src/components/CharacterAttributes.vue
@@ -17,8 +17,7 @@ const attributeRolls = defineModel<Attributes>("attributeRolls", { required: tru
 const attributeAdvances = defineModel<Attributes>("attributeAdvances", { required: true });
 
 const contentContainerRef = ref(null);
-const smELSize = useElSize(ViewSize.sm, contentContainerRef);
-const mdELSize = useElSize(ViewSize.md, contentContainerRef);
+const lgELSize = useElSize(ViewSize.lg, contentContainerRef);
 
 const racial = computed(() => {
   return getAttributes(props.species);
@@ -32,7 +31,7 @@ const total = computed(() => {
 <template>
   <div class="mb-1">{{ title }}</div>
   <div ref="contentContainerRef" class="w-full">
-    <div class="bg-neutral-50 rounded-xl border border-neutral-300 min-w-fit w-full">
+    <div v-if="!lgELSize.isEqualOrGreater.value" class="bg-neutral-50 rounded-xl border border-neutral-300 w-full">
       <table class="w-full">
         <thead>
           <tr class="text-left">
@@ -42,7 +41,7 @@ const total = computed(() => {
           </tr>
         </thead>
         <tbody>
-          <tr class="bg-white hover:bg-neutral-200">
+          <tr class="bg-white">
             <td class="py-2 pl-2 pr-1 border-b border-neutral-300">Rolls</td>
             <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
               <FormInput type="number" :modelValue="attributeRolls.WS" />
@@ -51,26 +50,26 @@ const total = computed(() => {
               <FormInput type="number" :modelValue="attributeRolls.WS" />
             </td>
           </tr>
-          <tr class="bg-white hover:bg-neutral-200">
+          <tr class="bg-white">
             <td class="py-2 pl-2 pr-1 border-b border-neutral-300">Racial</td>
-            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
               {{ racial.WS }}
             </td>
-            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
               {{ racial.BS }}
             </td>
           </tr>
-          <tr class="bg-white hover:bg-neutral-200">
+          <tr class="bg-white">
             <td class="py-2 pl-2 pr-1 border-b border-neutral-300">Other</td>
-            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
               {{ otherAttributes.WS }}
             </td>
-            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
               {{ otherAttributes.BS }}
             </td>
           </tr>
-          <tr class="bg-white hover:bg-neutral-200">
-            <td class="py-2 pl-2 pr-1 border-b border-neutral-300">Advances</td>
+          <tr class="bg-white">
+            <td class="py-2 pl-2 pr-1 border-b border-neutral-300">Adv</td>
             <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
               <FormInput type="number" :modelValue="attributeAdvances.WS" />
             </td>
@@ -78,13 +77,177 @@ const total = computed(() => {
               <FormInput type="number" :modelValue="attributeAdvances.WS" />
             </td>
           </tr>
-          <tr class="bg-white hover:bg-neutral-200">
+          <tr class="bg-white">
             <td class="py-2 pl-2 pr-1 border-b border-neutral-300">Sum</td>
             <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
-              <FormInput type="number" :modelValue="total.WS" />
+              {{ total.WS }}
             </td>
             <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
-              <FormInput type="number" :modelValue="total.WS" />
+              {{ total.BS }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="w-full">
+        <thead>
+          <tr class="text-left">
+            <th class="border-b border-neutral-300 py-2 px-2">S</th>
+            <th class="border-b border-neutral-300 py-2 px-2">T</th>
+            <th class="border-b border-neutral-300 py-2 px-2">I</th>
+            <th class="border-b border-neutral-300 py-2 px-2">Ag</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white">
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeRolls.S" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeRolls.T" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeRolls.I" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeRolls.Ag" />
+            </td>
+          </tr>
+          <tr class="bg-white">
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ racial.S }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ racial.T }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ racial.I }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ racial.Ag }}
+            </td>
+          </tr>
+          <tr class="bg-white">
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ otherAttributes.S }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ otherAttributes.T }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ otherAttributes.I }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ otherAttributes.Ag }}
+            </td>
+          </tr>
+          <tr class="bg-white">
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeAdvances.S" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeAdvances.T" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeAdvances.I" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeAdvances.Ag" />
+            </td>
+          </tr>
+          <tr class="bg-white">
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ total.S }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ total.T }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ total.I }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ total.Ag }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="w-full">
+        <thead>
+          <tr class="text-left">
+            <th class="border-b border-neutral-300 py-2 px-2">Dex</th>
+            <th class="border-b border-neutral-300 py-2 px-2">Int</th>
+            <th class="border-b border-neutral-300 py-2 px-2">WP</th>
+            <th class="border-b border-neutral-300 py-2 px-2">Fel</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white">
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeRolls.Dex" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeRolls.Int" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeRolls.WP" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeRolls.Fel" />
+            </td>
+          </tr>
+          <tr class="bg-white">
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ racial.Dex }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ racial.Int }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ racial.WP }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ racial.Fel }}
+            </td>
+          </tr>
+          <tr class="bg-white">
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ otherAttributes.Dex }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ otherAttributes.Int }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ otherAttributes.WP }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ otherAttributes.Fel }}
+            </td>
+          </tr>
+          <tr class="bg-white">
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeAdvances.Dex" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeAdvances.Int" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeAdvances.WP" />
+            </td>
+            <td class="py-2 px-1 border-b border-neutral-300 max-w-12">
+              <FormInput type="number" :modelValue="attributeAdvances.Fel" />
+            </td>
+          </tr>
+          <tr class="bg-white">
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ total.Dex }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ total.Int }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ total.WP }}
+            </td>
+            <td class="py-2 px-3 border-b border-neutral-300 max-w-12">
+              {{ total.Fel }}
             </td>
           </tr>
         </tbody>
