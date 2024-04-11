a92db2792db684e0b007e1a763998f7068949667
jacek <jacek.miloszewski@protonmail.com>
2024-04-11 15:54:49 +0100

    zxc

--- src/frontend/src/components/CharacterModifiersAttributes.vue
+++ src/frontend/src/components/CharacterModifiersAttributes.vue
@@ -34,28 +34,24 @@ const tdClass = ["px-2", "py-2", "border-b", "border-neutral-300"];
 </script>
 
 <template>
-  <div class="flex flex-col gap-4">
-    <div v-for="(row, index) in attributeNames" :key="index">
-      <div class="bg-neutral-50 rounded-xl border border-neutral-300 w-full">
-        <table class="w-full">
-          <thead>
-            <tr class="text-left">
-              <th v-for="attributeName in row" :key="attributeName" :class="thClass">
-                {{ attributeName }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="bg-white">
-              <td v-for="attributeName in row" :key="attributeName" :class="tdClass">
-                <slot :name="attributeName"></slot>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div class="bg-neutral-50 rounded-b-xl h-5 w-full"></div>
-      </div>
-    </div>
+  <div class="bg-neutral-50 rounded-xl border border-neutral-300 w-full">
+    <table v-for="(row, index) in attributeNames" :key="index" class="w-full">
+      <thead>
+        <tr class="text-left">
+          <th v-for="attributeName in row" :key="attributeName" :class="thClass">
+            {{ attributeName }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="bg-white">
+          <td v-for="attributeName in row" :key="attributeName" :class="tdClass">
+            <slot :name="attributeName"></slot>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="bg-neutral-50 rounded-b-xl h-5 w-full"></div>
   </div>
 </template>
 
