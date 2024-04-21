9dd3ad9e9eb13b8084bfafd3ffdb13f07ea8f2be
jacek <jacek.miloszewski@protonmail.com>
2024-04-21 10:29:12 +0100

    fix: bring back analytics

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -2,10 +2,18 @@ import App from "./App.vue";
 import { VueReCaptcha } from "vue-recaptcha-v3";
 import { createApp } from "vue";
 import router from "./router";
+import VueGtag from "vue-gtag";
 
 const app = createApp(App);
 
 app.use(router);
+
+app.use(VueGtag, {
+  bootstrap: false,
+  config: {
+    id: import.meta.env.VITE_ANALYTICS_ID,
+  },
+});
 
 VueReCaptcha.install(app, {
   siteKey: import.meta.env.VITE_RECAPTCHA_SITE_KEY,
