8faca278853dc04bfcb58c61f8e66cd2d0e3495f
jacek <jacek.miloszewski@protonmail.com>
2024-04-23 08:24:09 +0100

    fix: disable copy character if not logged in

--- src/frontend/src/components/ActionButtonsCharacter.vue
+++ src/frontend/src/components/ActionButtonsCharacter.vue
@@ -21,7 +21,7 @@ const auth = useAuth();
   <div class="flex gap-2 my-1">
     <ActionButton size="sm" @click="emit('view', id)">View</ActionButton>
     <ActionButton v-if="canEdit" size="sm" @click="emit('edit', id)">Edit</ActionButton>
-    <ActionButton v-if="auth.loggedIn" size="sm" variant="amber" @click="emit('copy', id)">Copy</ActionButton>
+    <ActionButton v-if="auth.loggedIn.value" size="sm" variant="amber" @click="emit('copy', id)">Copy</ActionButton>
     <ActionButton v-if="canEdit" size="sm" variant="red" class="mx-1" @click="emit('delete', id)">Delete</ActionButton>
   </div>
 </template>
