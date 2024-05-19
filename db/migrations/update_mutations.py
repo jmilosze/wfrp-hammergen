a5e1708e906ccac2d39fe0f35c1ba6d4792286a2
jacek <jacek.miloszewski@protonmail.com>
2024-05-19 16:16:03 +0100

    chore: migrate-db tools

--- db/migrations/update_mutations.py
+++ db/migrations/update_mutations.py
@@ -8,23 +8,15 @@ DB_NAME = os.environ["DB_NAME"]
 db = MongoClient(MONGO_URI, 27017).get_database(name=DB_NAME)
 collection = db.mutation
 
-modifiers = {
-    "size": 0,
-    "attributes": {
-        "WS": 0,
-        "BS": 0,
-        "S": 0,
-        "T": 0,
-        "I": 0,
-        "Ag": 0,
-        "Dex": 0,
-        "Int": 0,
-        "WP": 0,
-        "Fel": 0,
-    },
-    "movement": 0,
-}
+# items = collection.find()
+# for item in items:
+#     collection.find_one_and_update({"_id": item["_id"]}, {"$set": {"modifiers": modifiers}})
 
-items = collection.find()
-for item in items:
-    collection.find_one_and_update({"_id": item["_id"]}, {"$set": {"modifiers": modifiers}})
+result = collection.update_many(
+    {},  # This filter matches all documents
+    {
+        "$set": {"object.modifiers.effects": []}  # Adds 'effects' as an empty array inside 'modifiers'
+    }
+)
+
+print(f"Documents modified: {result.modified_count}")
