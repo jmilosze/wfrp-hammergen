cf91e09918564d87b2e14ae9b81733dc80723b17
Jacek Miloszewski <jacek.miloszewski@protonmail.com>
2024-07-07 23:26:08 +0100

    feat: added service selector to config (#58)

--- src/api-go/cmd/wfrp/main.go
+++ src/api-go/cmd/wfrp/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/jmilosze/wfrp-hammergen-go/internal/config"
 	"github.com/jmilosze/wfrp-hammergen-go/internal/dependencies/gin"
 	"github.com/jmilosze/wfrp-hammergen-go/internal/dependencies/golangjwt"
 	"github.com/jmilosze/wfrp-hammergen-go/internal/dependencies/mailjet"
+	"github.com/jmilosze/wfrp-hammergen-go/internal/dependencies/memdb"
+	"github.com/jmilosze/wfrp-hammergen-go/internal/dependencies/mockcaptcha"
+	"github.com/jmilosze/wfrp-hammergen-go/internal/dependencies/mockemail"
 	"github.com/jmilosze/wfrp-hammergen-go/internal/dependencies/mongodb"
 	"github.com/jmilosze/wfrp-hammergen-go/internal/dependencies/recaptcha"
 	"github.com/jmilosze/wfrp-hammergen-go/internal/dependencies/validator"
+	"github.com/jmilosze/wfrp-hammergen-go/internal/domain"
+	"github.com/jmilosze/wfrp-hammergen-go/internal/domain/user"
+	wh "github.com/jmilosze/wfrp-hammergen-go/internal/domain/warhammer"
 	"github.com/jmilosze/wfrp-hammergen-go/internal/http"
 	"github.com/jmilosze/wfrp-hammergen-go/internal/services"
 	mock "github.com/jmilosze/wfrp-hammergen-go/test/mock_data"
@@ -30,15 +37,42 @@ func run() error {
 
 	val := validator.NewValidator()
 	jwtService := golangjwt.NewHmacService(cfg.Jwt.HmacSecret, cfg.Jwt.AccessExpiry, cfg.Jwt.ResetExpiry)
-	emailService := mailjet.NewEmailService(cfg.Email.FromAddress, cfg.Email.PublicApiKey, cfg.Email.PrivateApiKey)
-	captchaService := recaptcha.NewCaptchaService(cfg.Captcha.Secret, cfg.Captcha.Url, cfg.Captcha.MinScore, cfg.Captcha.Timeout)
-	mongoDbService := mongodb.NewDbService(cfg.MongoDb.Uri, cfg.MongoDb.Name)
-	defer mongoDbService.Disconnect()
 
-	userDbService := mongodb.NewUserDbService(mongoDbService, cfg.MongoDb.CreateUserIndexes)
+	var emailService domain.EmailService
+	if cfg.Services.Email == "mailjet" {
+		emailService = mailjet.NewEmailService(cfg.Email.FromAddress, cfg.Email.PublicApiKey, cfg.Email.PrivateApiKey)
+	} else if cfg.Services.Email == "mockemail" {
+		emailService = mockemail.NewEmailService(cfg.Email.FromAddress)
+	} else {
+		return fmt.Errorf("unknown email service: %s", cfg.Services.Email)
+	}
+
+	var captchaService domain.CaptchaService
+	if cfg.Services.Captcha == "recaptcha" {
+		captchaService = recaptcha.NewCaptchaService(cfg.Captcha.Secret, cfg.Captcha.Url, cfg.Captcha.MinScore, cfg.Captcha.Timeout)
+	} else if cfg.Services.Captcha == "mockcaptcha" {
+		captchaService = mockcaptcha.NewCaptchaService()
+	} else {
+		return fmt.Errorf("unknown captcha service: %s", cfg.Services.Captcha)
+	}
+
+	var userDbService user.UserDbService
+	var whDbService wh.WhDbService
+
+	if cfg.Services.Db == "mongodb" {
+		mongoDbService := mongodb.NewDbService(cfg.MongoDb.Uri, cfg.MongoDb.Name)
+		defer mongoDbService.Disconnect()
+
+		userDbService = mongodb.NewUserDbService(mongoDbService, cfg.MongoDb.CreateUserIndexes)
+		whDbService = mongodb.NewWhDbService(mongoDbService, cfg.MongoDb.CreateWhIndexes)
+	} else if cfg.Services.Db == "memdb" {
+		userDbService = memdb.NewUserDbService()
+		whDbService = memdb.NewWhDbService()
+	} else {
+		return fmt.Errorf("unknown database service: %s", cfg.Services.Db)
+	}
+
 	userService := services.NewUserService(&cfg.UserService, userDbService, emailService, jwtService, val)
-
-	whDbService := mongodb.NewWhDbService(mongoDbService, cfg.MongoDb.CreateWhIndexes)
 	whService := services.NewWhService(val, whDbService)
 
 	ctx, cancel := context.WithTimeout(context.Background(), cfg.Server.RequestTimeout)
