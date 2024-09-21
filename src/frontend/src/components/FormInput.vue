6196d80ef26df3662eac1afb86f0d7afd13f4960
jacek <jacek.miloszewski@protonmail.com>
2024-09-22 00:25:01 +0100

    chore: fix eslint fixes

--- src/frontend/src/components/FormInput.vue
+++ src/frontend/src/components/FormInput.vue
@@ -37,10 +37,10 @@ const value = computed({
         <input
           v-model="value"
           :type="type ? type : 'text'"
-          class="border border-neutral-300 rounded w-full h-10 px-2 focus:outline-neutral-700 focus:border-transparent focus:outline-2"
+          class="border border-neutral-300 rounded w-full h-10 px-2 focus:outline-neutral-700 focus:border-transparent focus:outline-2 focus:outline"
           :class="centerText ? ['text-center'] : []"
           :disabled="disabled ? disabled : false"
-        >
+        />
       </div>
       <div>
         <slot />
