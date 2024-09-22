920e5e316f53a15a959d58a93f01a72c8cc0207c
jacek <jacek.miloszewski@protonmail.com>
2024-09-22 10:51:44 +0100

    chore: eslint

--- src/frontend/eslint.config.js
+++ src/frontend/eslint.config.js
@@ -17,7 +17,8 @@ export default [
       "vue/attribute-hyphenation": [1, "never"],
       "vue/v-on-event-hyphenation": [1, "never"],
       "vue/max-attributes-per-line": "off",
-      "vue/singleline-html-element-content-newline": "off"
+      "vue/singleline-html-element-content-newline": "off",
+      "vue/html-self-closing": "off",
     },
   },
 ];
