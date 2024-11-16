f1548fadad1c22c0dfa87caa5c449d91223ca795
jacek <jacek.miloszewski@protonmail.com>
2024-11-16 11:09:12 +0000

    feat: add new query params to mutations

--- src/frontend/src/views/Warhammer/List/ListMutations.vue
+++ src/frontend/src/views/Warhammer/List/ListMutations.vue
@@ -6,30 +6,26 @@ import TableWithSearch from "../../../components/TableWithSearch.vue";
 import Header from "../../../components/PageHeader.vue";
 import { addSpaces } from "../../../utils/string.ts";
 import { source } from "../../../services/wh/source.ts";
-import { computed, ref, watch } from "vue";
+import { computed } from "vue";
 import { ViewSize } from "../../../utils/viewSize.ts";
 import ActionButtonsNonCharacter from "../../../components/ActionButtonsNonCharacter.vue";
-import { useRouter } from "vue-router";
+
 import DeleteModal from "../../../components/DeleteModal.vue";
-import { getOptions, queryParamsFromRouterQuery, queryParamsToRouterQuery } from "../../../utils/whList.ts";
+import { getListOfAllValues, getOptions } from "../../../utils/whList.ts";
 import SelectInput from "../../../components/SelectInput.vue";
 import { useAuth } from "../../../composables/auth.ts";
 import AlertBlock from "../../../components/AlertBlock.vue";
 import LinkButton from "../../../components/LinkButton.vue";
+import { useQueryParams } from "../../../composables/useQueryParams.ts";
 
 const whList = useWhList(new MutationApi(authRequest));
 await whList.loadWhList();
 
-const router = useRouter();
-const queryParams = ref({ search: "", source: "", type: "" });
-queryParamsFromRouterQuery(queryParams.value, router.currentRoute.value.query);
-watch(
-  () => queryParams,
-  (newValue) => {
-    router.replace({ query: queryParamsToRouterQuery(newValue.value) });
-  },
-  { deep: true },
-);
+const allMutationTypes = getListOfAllValues(mutationTypeList);
+
+const searchTerm = useQueryParams("search");
+const sourceTerm = useQueryParams("source", whList.sourceValues);
+const typeTerm = useQueryParams("type", allMutationTypes);
 
 const auth = useAuth();
 
@@ -43,8 +39,8 @@ const columns = [
 
 const items = computed(() => {
   return whList.whList.value
-    .filter((wh) => queryParams.value.source === "" || queryParams.value.source in wh.source)
-    .filter((wh) => queryParams.value.type === "" || queryParams.value.type === wh.type.toString())
+    .filter((wh) => sourceTerm.value === "" || sourceTerm.value in wh.source)
+    .filter((wh) => typeTerm.value === "" || typeTerm.value === wh.type.toString())
     .map((x) => formatMutationRow(x))
     .sort((a, b) => a.name.localeCompare(b.name));
 });
@@ -83,16 +79,10 @@ const filteredTypeOptions = computed(() => {
   </AlertBlock>
   <Header title="Mutations" />
   <div class="flex flex-wrap justify-between">
-    <SelectInput v-model="queryParams.source" :options="whList.filteredSourceOptions.value" class="grow mb-2 mx-1" />
-    <SelectInput v-model="queryParams.type" :options="filteredTypeOptions" class="grow mb-2 mx-1" />
+    <SelectInput v-model="sourceTerm" :options="whList.filteredSourceOptions.value" class="grow mb-2 mx-1" />
+    <SelectInput v-model="typeTerm" :options="filteredTypeOptions" class="grow mb-2 mx-1" />
   </div>
-  <TableWithSearch
-    v-model="queryParams.search"
-    :fields="columns"
-    :items="items"
-    :stackedViewSize="ViewSize.lg"
-    class="mx-1"
-  >
+  <TableWithSearch v-model="searchTerm" :fields="columns" :items="items" :stackedViewSize="ViewSize.lg" class="mx-1">
     <LinkButton
       v-if="auth.loggedIn.value"
       class="mr-2 mb-2 shrink-0 btn"
