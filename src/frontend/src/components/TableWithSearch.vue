017156e4577c63f15332f2eb455ad4ac4b447f42
jacek <jacek.miloszewski@gmail.com>
2026-09-19 23:56:04 +0100

    chore: fix table column width

--- src/frontend/src/components/TableWithSearch.vue
+++ src/frontend/src/components/TableWithSearch.vue
@@ -134,7 +134,12 @@ onUpdated(() => {
           <table v-if="isEqualOrGreater" class="w-full">
             <thead>
               <tr class="text-left">
-                <th v-for="field in fields" :key="field.name" class="border-b border-neutral-300 py-2 px-5">
+                <th
+                  v-for="field in fields"
+                  :key="field.name"
+                  class="border-b border-neutral-300 py-2 px-5"
+                  :class="[field.name === 'name' ? 'min-w-48 max-w-xs' : '']"
+                >
                   {{ field.displayName }}
                 </th>
               </tr>
@@ -145,7 +150,10 @@ onUpdated(() => {
                   v-for="field in fields"
                   :key="field.name"
                   class="py-2 px-5 border-b border-neutral-300"
-                  :class="[field.name === 'name' || field.name === 'description' ? '[overflow-wrap:anywhere]' : '']"
+                  :class="[
+                    field.name === 'name' ? 'min-w-48 max-w-xs wrap-break-word' : '',
+                    field.name === 'description' ? 'wrap-break-word' : '',
+                  ]"
                 >
                   <slot :name="field.name" v-bind="item">{{ String(item[field.name]) }}</slot>
                 </td>
@@ -164,9 +172,10 @@ onUpdated(() => {
                   <div
                     v-for="field in fields"
                     :key="field.name"
-                    class="py-2 px-5 border-b border-neutral-300 flex items-center gap-2 [overflow-wrap:anywhere]"
+                    class="py-2 px-5 border-b border-neutral-300 flex items-center gap-2"
+                    :class="[field.name === 'name' || field.name === 'description' ? 'wrap-break-word' : '']"
                   >
-                    <div v-if="!field.skipStackedTitle" class="font-bold">{{ field.displayName }}</div>
+                    <div v-if="!field.skipStackedTitle" class="font-bold shrink-0">{{ field.displayName }}</div>
                     <slot :name="field.name" v-bind="item">{{ String(item[field.name]) }}</slot>
                   </div>
                   <div class="border-b-4 border-neutral-400" />
